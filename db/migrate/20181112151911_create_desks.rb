507bdb264a7f2d223e8cba56f57d230ea3bdd93a
Florent Legeron <legeronflo@hotmail.fr>
2018-11-12 17:04:32 +0100

    models created

---
+++ db/migrate/20181112151911_create_desks.rb
@@ -0,0 +1,12 @@
+class CreateDesks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :desks do |t|
+      t.decimal :price
+      t.string :title
+      t.string :description
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20181112152208_create_bookings.rb
@@ -0,0 +1,14 @@
+class CreateBookings < ActiveRecord::Migration[5.2]
+  def change
+    create_table :bookings do |t|
+      t.references :user, foreign_key: true
+      t.references :desk, foreign_key: true
+      t.date :start_at
+      t.datetime :end_at
+      t.string :review
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
