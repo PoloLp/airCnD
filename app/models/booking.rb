ca04347cd096c4af839b12d441c538f4f0231185
Florent Legeron <legeronflo@hotmail.fr>
2018-11-13 11:16:34 +0100

    testo ok for models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :desk
-  validates :price, presence: true
   validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }
   validates :start_at, presence: true
   validates :end_at, presence: true
