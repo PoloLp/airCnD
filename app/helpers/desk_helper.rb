44004f9baada79c86a5d0e02bcfab6ab2ce2a7ba
Florent Legeron <legeronflo@hotmail.fr>
2018-11-15 11:57:05 +0100

    scss for map and booking show responsive

--- app/helpers/desk_helper.rb
+++ app/helpers/desk_helper.rb
@@ -1,7 +1,15 @@
 module DeskHelper
   def safe_desk_img_path(desk)
     if desk.photo.file
-      return cl_image_path desk.photo, height: 300, width: 400, crop: :fill
+      return cl_image_path desk.photo, height: 600, width: 900, crop: :fill
+    else
+      return ''
+    end
+  end
+
+  def safe_desk_img_large_path(desk)
+    if desk.photo.file
+      return cl_image_path desk.photo, height: 300, width: 700, crop: :scale
     else
       return ''
     end
