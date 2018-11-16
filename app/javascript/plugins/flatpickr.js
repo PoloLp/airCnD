d3c0ebf7164b8c51bb1a17ef96dbfdb8025bfa56
Paul Antoine Hocke <hocke.p.a@gmail.com>
2018-11-16 09:14:57 +0100

    add blue theme for the date picker

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,7 +1,6 @@
 // app/javascript/plugins/flatpickr.js
 import flatpickr from "flatpickr"
-
-import "flatpickr/dist/flatpickr.min.css" // Note this is important!
+import "flatpickr/dist/themes/material_blue.css" // A path to the theme CSS
 
 flatpickr(".datepicker", {
   enableTime: true,
